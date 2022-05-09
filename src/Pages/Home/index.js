f8584f3f00c4dfbde5e4db9812c0cd520cb52e2c
Luiz <luizsaulo@yahoo.com.br>
2022-05-09 11:18:24 -0300

    adding reservation property to work

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { MdFlightTakeoff } from 'react-icons/md';
 
 import api from '../../services/api';
 import './style.css';
 
 export default function Home() {
+  const dispatch = useDispatch();
   const [trips, setTrips] = useState([]);
 
   useEffect(()=>{
@@ -17,6 +19,13 @@ export default function Home() {
 
   }, []);
 
+  function handleAdd(trip){
+    dispatch({
+      type: 'AAD_RESERVE',
+      trip
+    });
+  }
+
   return (
     <div>
        <div className='box'>
@@ -28,7 +37,7 @@ export default function Home() {
 
             <button
             type='button'
-            onClick={()=> {}}
+            onClick={()=> handleAdd(trip) }
             >
               <div>
                 <MdFlightTakeoff size={16} color='#fff' />
