2cdf87d5a915ad5be03b75ad44527d8855440aef
Luiz <luizsaulo@yahoo.com.br>
2022-05-09 15:48:48 -0300

    implementing the reservation button with addition of reserves

--- src/Pages/Reservas/index.js
+++ src/Pages/Reservas/index.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import { MdDelete } from 'react-icons/md';
-import './style.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { MdDelete } from "react-icons/md";
+import "./style.css";
 
 export default function Reservas() {
+  const reserves = useSelector((state) => state.reserve);
+
   return (
     <div>
-        <h1 className='title'>Você solicitou 1 reservas</h1>
+      <h1 className="title">Você solicitou {reserves.length} reservas</h1>
 
-        <div className='reservas'>
+      {reserves.map((reserve) => (
+        <div className="reservas" key={reserve.id}>
           <img
-          src='https://sujeitoprogramador.com/wp-copntent/uploads/2019/12/maceio.jpg'
-          alt='Maceió'
+            src={reserve.image}
+            alt={reserve.title}
           />
-          <strong>Viagem Maceió 7 dias</strong>
+          <strong>{reserve.title}</strong>
           <span>Quantidade: 2</span>
-          <button
-          type='button'
-          onClick={()=> {}}
-          >
-            <MdDelete size={20} color='#191919' />
-          </button>          
+          <button type="button" onClick={() => {}}>
+            <MdDelete size={20} color="#191919" />
+          </button>
         </div>
+      ))}
 
-        <footer>
-          <button type='button'>Solicitar Reservas</button>
-        </footer>
-        
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
     </div>
-  )
+  );
 }
